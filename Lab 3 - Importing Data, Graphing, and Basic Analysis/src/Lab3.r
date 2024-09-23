23c0662c0c5e7a68cbb215db5e9c4594d8e3cf9f
Vaney <v.vaney@ufl.edu>
2024-09-23 17:42:00 -0400

    yeee

--- Lab 3 - Importing Data, Graphing, and Basic Analysis/src/Lab3.r
+++ Lab 3 - Importing Data, Graphing, and Basic Analysis/src/Lab3.r
@@ -11,8 +11,8 @@
 ##	Load and prepare required data:
 ###################################
 
-lifedata <- read.table(file.choose(), sep="\t", header=FALSE, stringsAsFactors = T)
-lifedata
+lifedata <- read.table(file.choose("Lab 3 - Importing Data, Graphing, and Basic Analysis/data/TimeInLifeData.txt"), sep="\t", header=FALSE, stringsAsFactors = T)
+
 head(lifedata, 10)
 
 ## Set column names
@@ -31,15 +31,20 @@ lifedata[rowSums(is.na(lifedata)) > 0,]
 ##  Fix the dataset errors
 na.omit(lifedata)
 lifedata <- lifedata[rowSums(is.na(lifedata)) < 2,]
+
 summary(lifedata)
 lifedata[rowSums(is.na(lifedata)) > 0,]
-lifedata$bodytemp[is.na(lifedata$bodytemp)] <- median(lifedata$bodytemp, na.rm = TRUE)
-outs <- boxplot.stats(lifedata$bodytemp)$out
+lifedata$bodytemp[is.na(lifedata$bodytemp)] <- median(lifedata$bodytemp, na.rm = TRUE) # making all 0's the mean
+
+outs <- boxplot.stats(lifedata$bodytemp)$out #finds the outlier
 lifedata$bodytemp[lifedata$bodytemp %in% outs] <- median(lifedata$bodytemp, na.rm = TRUE)
+
 summary(lifedata)
 lifedata$age[lifedata$age == 1430] <- median(lifedata$age, na.rm = TRUE)
+
+
 summary(lifedata)
-lifedata <- droplevels(lifedata, exclude = "")
+lifedata <- droplevels(lifedata, exclude = "") #exclude any empty string element 
 summary(lifedata)
 
 
@@ -48,25 +53,41 @@ summary(lifedata)
 ###########################################################
 
 ## Set up storage of all following figures in a pdf doc and tabular info in a text file
-## Change these file directories to location of your output folder for lab 3!!!
-pdf(file="/Volumes/KINGSTON UF/KINGSTON COPY/R Class/IntroR /Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convars.pdf")
-sink("/Volumes/KINGSTON UF/KINGSTON COPY/R Class/IntroR /Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convars.txt")
+## Change these file directories to location of your output folder for lab 3!!
+      ###This command opens a PDF device, which means that any plots created after this
+          #line will be saved to a file named convars.pdf in the specified directory 
+
+pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convars.pdf")
+sink("Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convars.txt")
 
 ## Set the graphics device to plot two figures in a 1 row by 2 column configuration
 par(mfrow=c(1,2))
+
 ## Create histogram of dependent variable (the difference measure)
 hist(lifedata$diff, xlab="Difference Measure", main=NA, col="skyblue")
+
+
 ## Add a one dimensional representation of the data along the x axis
 rug(lifedata$diff)
+
+
 ## Draw a bounding box around the histogram plot
 box()
+
+
 ## Create a box plot of the dependent variable
 boxplot(lifedata$diff, ylab="Difference Measure", main=NA, 
 		col="skyblue")
+
+
 ## Restore the graphics device to single plot configuration
 par(mfrow=c(1,1))
+
+
 ## Add a title to the plot
 title(main="Difference Measure")
+
+
 
 ## Create a data frame with dependent variable summary statistics
 diff<-data.frame(n=length(lifedata$diff), min=min(lifedata$diff), 
@@ -78,29 +99,56 @@ diff<-data.frame(n=length(lifedata$diff), min=min(lifedata$diff),
 print("Diff")
 print(diff, digits=3)
 
+## Stop the sink and close the graphics device
+ sink()
+ dev.off()
 #########################################################################
 ##  Let's test your coding skills a little!
 ##  Create a histogram, boxplot, and statistical summary for the age independent variable
 ##  HINT:  This should be very similar to the code above for the diff variable
 #########################################################################
 
+#pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/age.pdf")
+#sink("Lab 3 - Importing Data, Graphing, and Basic Analysis/output/age.txt")
+
+
+par(mfrow=c(1,2))
+
+## Create histogram of dependent variable (the difference measure)
+hist(lifedata$age, xlab="Age", main=NA, col="skyblue")
+
+
+rug(lifedata$age)
+
+
+
+box()
+
+
+
+boxplot(lifedata$age, ylab="Age", main=NA, 
+        col="skyblue")
 
 
 
 
+par(mfrow=c(1,1))
+title(main="Age Destribution")
 
+age<-data.frame(n=length(lifedata$age), min=min(lifedata$age), 
+                 max=max(lifedata$age), mean=mean(lifedata$age), 
+                 median=median(lifedata$age), var=var(lifedata$age), 
+                 sd=sd(lifedata$age))
 
-
-
-
-
-
+## Print the data frame, rounded to three digits 
+print(age)
+print(age, digits=3)
 
 
 
 ## Stop the sink and close the graphics device
-sink()
-dev.off()
+#sink()
+#dev.off()
 ## Remove age and diff objects from workspace
 rm(age)
 rm(diff)
@@ -112,8 +160,8 @@ rm(diff)
 
 ## Set up storage of all following figures in a pdf doc and tabular info in a text file
 ## Change these file directories to location of your output folder for lab 3!!!
-pdf(file="/Volumes/KINGSTON UF/KINGSTON COPY/R Class/IntroR /Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvars.pdf")
-sink("/Volumes/KINGSTON UF/KINGSTON COPY/R Class/IntroR /Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvars.txt")
+#pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvars.pdf")
+#sink("Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvars.txt")
 
 
 ## cat is simpler alternative to print
@@ -155,8 +203,8 @@ box()
 par(mfrow=c(1,1))
 
 ## Stop the sink and close the graphics device
-sink()
-dev.off()
+#sink()
+#dev.off()
 
 
 ####################################################################################
@@ -181,6 +229,7 @@ edu = factor(edu, labels=c("HS", "UGrad", "Grad"))
 
 ## Create data frames for the dependent variable and categorical IVs
 lifedata.dv<-diff
+
 lifedata.cat <- data.frame(sex, marriage, job, family, edu)
 
 ##Create vector of Variable Names
@@ -236,8 +285,8 @@ concatfun<-function(eachvar, name, response){
 
 ## Set up storage of all following figures in a pdf doc and tabular info in a text file
 ## Change these file directories to location of your output folder for lab 3!!!
-pdf(file="/Volumes/KINGSTON/R Class/Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvarstats.pdf")
-sink("/Volumes/KINGSTON/R Class/Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvarstats.txt")
+#pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvarstats.pdf")
+#sink("Lab 3 - Importing Data, Graphing, and Basic Analysis/output/catvarstats.txt")
 
 ##  Loop for sending categorical variables to categorical analysis function defined above
 ##  loop iterates through the length of the cat variable data frame, pulling out one 
@@ -250,8 +299,8 @@ for (i in 1:length(lifedata.cat)) {
 }
 
 ## Stop the sink and close the graphics device
-sink()
-dev.off()
+# sink()
+# dev.off()
 
 ##########################################################################
 ## Continuous Variables 	 
@@ -266,7 +315,7 @@ lifedata.con <- data.frame(age, bodytemp, heartrate, support, health)
 vars.con<-colnames(lifedata.con)
 
 ## Plot all continuous DVs vs. IV
-pdf(file="/Volumes/KINGSTON UF/KINGSTON COPY/R Class/IntroR /Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarscatter.pdf")
+#pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarscatter.pdf")
 ## Set up graphics device to display a 2 row by 3 column configuration of plots
 par(mfrow=c(2, 3))
 
@@ -280,49 +329,50 @@ for (i in 1:length(lifedata.con)) {
 ## Restore the graphics device to single plot configuration
 par(mfrow=c(1,1)) 
 
-dev.off()
+#dev.off()
 
 
 ## Function for analysis of continuous IVs against continuous DV
+
 conconfun<-function(eachvar, name, response){
-		## Create nice text header for stats output
-		header<-paste("##########", name, "##########")
-		## print the nice header
-		print(header)
-		## print the results of a Pearson's Correlation
-		print(cor.test(response, eachvar))
-		## Plot IV vs DV as scatter plot
-		plot(eachvar, response, xlab=name, ylab="Difference Measure", type="p", pch=19, col="black")
-		## Fit a linear model
-		fit<-lm(response~eachvar)
-		## Add red line of model fit to plot
-		abline(fit, col="red", lwd=2)
-		## Create summary of model fit
-		fitsum<-summary(fit)
-		## print model summary
-		print(fitsum)
-		## if else similar to previous cat function to parse out p values
-		if(fitsum$coef[2,4]<0.001){
-			pval<-"<0.001"
-		} else{
-			pval<-round(fitsum$coef[2,4], digits=4)
-		}
-		## create text string for adjusted r squared and p value
-		labtext<-paste("Adj. R^2:", round(fitsum$adj.r.squared, digits=4), "p-value:", pval)
-		## add text string to plot
-		mtext(labtext, side=3) 
-		## Set graphics device using layout command to a 2x2 grid that will be populated by 4 figures
-		layout(matrix(c(1,2,3,4),2,2))
-		## Plot the model diagnostics
-		plot(fit)
-		## Reset graphics device
-		par(mfrow=c(1,1))
-}
+  ## Create nice text header for stats output
+  header<-paste("##########", name, "##########")
+  ## print the nice header
+  print(header)
+  ## print the results of a Pearson's Correlation
+  print(cor.test(response, eachvar))
+  ## Plot IV vs DV as scatter plot
+  plot(eachvar, response, xlab=name, ylab="Difference Measure", type="p", pch=19, col="black")
+  ## Fit a linear model
+  fit<-lm(response~eachvar)
+  ## Add red line of model fit to plot
+  abline(fit, col="red", lwd=2)
+  ## Create summary of model fit
+  fitsum<-summary(fit)
+  ## print model summary
+  print(fitsum)
+  ## if else similar to previous cat function to parse out p values
+  if(fitsum$coef[2,4]<0.001){
+    pval<-"<0.001"
+  } else{
+    pval<-round(fitsum$coef[2,4], digits=4)
+  }
+  ## create text string for adjusted r squared and p value
+  labtext<-paste("Adj. R^2:", round(fitsum$adj.r.squared, digits=4), "p-value:", pval)
+  ## add text string to plot
+  mtext(labtext, side=3) 
+  ## Set graphics device using layout command to a 2x2 grid that will be populated by 4 figures
+  layout(matrix(c(1,2,3,4),2,2))
+  ## Plot the model diagnostics
+  plot(fit)
+  ## Reset graphics device
+  par(mfrow=c(1,1))
+} 
 
 ## Set up storage of all following figures in a pdf doc and tabular info in a text file
 ## Change these file directories to location of your output folder for lab 3!!!
-pdf(file="/Volumes/KINGSTON/R Class/Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarstats.pdf")
-sink("/Volumes/KINGSTON/R Class/Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarstats.txt")
+pdf(file="Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarstats.pdf")
+sink("Lab 3 - Importing Data, Graphing, and Basic Analysis/output/convarstats.txt")
 
 #########################################################################
 ##  Let's test your looping skills a little!
@@ -334,8 +384,24 @@ sink("/Volumes/KINGSTON/R Class/Lab 3 - Importing Data, Graphing, and Basic Anal
 ##Loop for sending continuous variables to continuous analysis function	
 
 
+for (i in 1:length(lifedata.con)) {
+  eachvar <- lifedata.con[,i]
+  name <- vars.con[i]
+  conconfun(eachvar, name, lifedata.dv)
+}
 
+## Set up graphics device to display a 2 row by 3 column configuration of plots
+par(mfrow=c(2, 3))
 
+## Loop through con variables, plotting each against DV
+for (i in 1:length(lifedata.con)) {
+  eachvar <- lifedata.con[,i]
+  name <- vars.con[i]
+  plot(eachvar, lifedata.dv, xlab=name, ylab="Difference Measure")
+}
+
+## Restore the graphics device to single plot configuration
+par(mfrow=c(1,1)) 
 
 ## Stop the sink and close the graphics device
 sink()
